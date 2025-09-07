cab8f97de2629fdbdd6be2c822ef711e57d23016
Vryon Antonio <vryonantonio@gmail.com>
2025-09-07 10:17:19 +0800

    Receipt layout update

--- src/Form.tsx
+++ src/Form.tsx
@@ -14,25 +14,34 @@ function Form({
   const [grossAmount, setGrossAmount] = useState(0);
   const [pax, setPax] = useState(0);
   const [discountNum, setDiscountNum] = useState(0);
-  const [woSc, setWoSc] = useState(0);
-  const [sc, setSc] = useState(0);
-  const [final, setFinal] = useState(0);
+  const [withoutServiceCharge, setWithoutServiceCharge] = useState(0);
+  const [serviceCharge, setServiceCharge] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [vatAndScDiscount, setVatAndScDiscount] = useState(0);
 
   function handleDiscount() {
     if (pax === 0) return alert("Please include number of pax.");
 
-    const divide = grossAmount / pax;
-    const vat = divide / vatPer;
-    const twentyP = vat - vat * 0.2;
-    const yesRes = twentyP * discountNum;
-    const noDisNum = pax - discountNum;
-    const noRes = divide * noDisNum;
-    const sumYesNo = yesRes + noRes;
-    setWoSc(sumYesNo);
-    const serviceCharge = (grossAmount / vatPer) * scPer;
-    setSc(serviceCharge);
-    const finalResult = sumYesNo + serviceCharge;
-    setFinal(finalResult);
+    const amountPerPerson = grossAmount / pax;
+    const baseAmountExcludingVAT = amountPerPerson / vatPer;
+    const discountedAmountPerPerson =
+      baseAmountExcludingVAT - baseAmountExcludingVAT * 0.2;
+
+    const totalDiscountedGroupAmount = discountedAmountPerPerson * discountNum;
+    setVatAndScDiscount(totalDiscountedGroupAmount);
+    const totalNonDiscountedGroupAmount = amountPerPerson * (pax - discountNum);
+
+    const totalBeforeServiceCharge =
+      totalDiscountedGroupAmount + totalNonDiscountedGroupAmount;
+    setWithoutServiceCharge(totalBeforeServiceCharge);
+
+    const serviceChargeAmount = (grossAmount / vatPer) * scPer;
+    setServiceCharge(serviceChargeAmount);
+
+    const finalBillAmount = totalBeforeServiceCharge + serviceChargeAmount;
+    setTotalAmount(finalBillAmount);
+
+    console.log(totalDiscountedGroupAmount);
   }
 
   return (
@@ -95,14 +104,48 @@ function Form({
         Submit
       </button>
 
+      {(formName === "Vat and SC" || formName === "Without Vat") && (
+        <>
+          <p className="flex justify-between">
+            <span>Amount without SC: </span>{" "}
+            <span> {grossAmount.toFixed(2)} </span>
+          </p>
+          <p className="flex justify-between">
+            <span>Regular Payment: </span>
+            <span>
+              {" "}
+              {(withoutServiceCharge - vatAndScDiscount).toFixed(2)}{" "}
+            </span>
+          </p>
+        </>
+      )}
+
+      {formName !== "Without Vat and SC" && (
+        <p className="flex justify-between">
+          <span>Net of Discount: </span>
+          <span>{vatAndScDiscount.toFixed(2)}</span>
+        </p>
+      )}
+
       <p className="flex justify-between">
-        <span>Without Service Charge: </span> <span> {woSc.toFixed(2)} </span>
+        <span>Amount: </span> <span> {withoutServiceCharge.toFixed(2)} </span>
       </p>
-      <p className="flex justify-between">
-        <span>Service Charge: </span> <span> {sc.toFixed(2)} </span>
-      </p>
-      <p className="flex justify-between">
-        <span>Amount: </span> <span> {final.toFixed(2)} </span>
+
+      {formName === "Without Vat and SC" && (
+        <p className="flex justify-between">
+          <span>Discount Amount: </span>{" "}
+          <span> {(grossAmount - totalAmount).toFixed(2)} </span>
+        </p>
+      )}
+
+      {(formName === "Vat and SC" || formName === "Without Vat") && (
+        <p className="flex justify-between">
+          <span>Service Charge: </span>{" "}
+          <span> {serviceCharge.toFixed(2)} </span>
+        </p>
+      )}
+      <p className="flex justify-between text-lg font-semibold">
+        <span>Total Amount: </span> <span> {totalAmount.toFixed(2)} </span>
       </p>
     </form>
   );
